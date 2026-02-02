5ff133818eaf3371fb0e6cbaa53e12f2e13ede74
sa-y-em <sayemmir100@gmail.com>
2026-02-02 13:58:24 +0600

    sum of only even number

--- function_problems.js
+++ function_problems.js
@@ -16,23 +16,45 @@
 
 //return even numbers only from a function
 //----------------------------------------------
+// function even(inputA) {
+//     let finalEven = []
+//     let finalOdd = []
+//     for (let i of inputA) {
+//         if (i % 2 === 0) {
+//             finalEven.push(i)
+
+
+//         }
+//         else {
+//             finalOdd.push(i)
+//         }
+//     }
+//     return { finalEven, finalOdd }
+// }
+
+// let a = even([2, 5, 65, 41, 23, 201, 222, 8520, 136])
+// console.log(a)
+// let b = even([4,10 , 1, 664, 8, 54, 555, 7, 121, 0, 5545, 10, 41])
+// console.log(b)
+//----------------------------------------------------------------------------
+//sum of only even numbers
+
 function even(inputA) {
-    let finalEven = []
-    let finalOdd = []
+    
+    let sum = 0;
     for (let i of inputA) {
         if (i % 2 === 0) {
-            finalEven.push(i)
+            // finalEven.push(i)
+            sum = sum + i
 
 
         }
-        else {
-            finalOdd.push(i)
-        }
+        
     }
-    return { finalEven, finalOdd }
+    return sum;
 }
 
-let a = even([2, 5, 65, 41, 23, 201, 222, 8520, 136])
+let a = even([2, 5, 65, 41, 23, 201, 222, 85201, 13])
 console.log(a)
-let b = even([4,010 , 1, 664, 8, 54, 555, 7, 121, 0, 5545, 10, 41])
+let b = even([4, 10, 1, 664, 8, 54, 555, 7, 121, 0, 5545, 10, 41])
 console.log(b)
