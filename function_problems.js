666de9b21fc4b60c98afafc2b5c77e0dd4e49042
sa-y-em <sayemmir100@gmail.com>
2026-02-02 13:23:16 +0600

    return even and odd nums from an array using a function

--- function_problems.js
+++ function_problems.js
@@ -1,14 +1,38 @@
 
 
 // sum of an array with a function
-function sum(inp) {
-    let initialSum= 0;
-    for(let i of inp){
-      initialSum = i + initialSum;     
+//-----------------------------------------------
+// function sum(inp) {
+//     let initialSum= 0;
+//     for(let i of inp){
+//       initialSum = i + initialSum;     
+//     }
+//     return initialSum;
+// }
+
+
+// let i = sum([50,5])
+// console.log(i)
+
+//return even numbers only from a function
+//----------------------------------------------
+function even(inputA) {
+    let finalEven = []
+    let finalOdd = []
+    for (let i of inputA) {
+        if (i % 2 === 0) {
+            finalEven.push(i)
+
+
+        }
+        else {
+            finalOdd.push(i)
+        }
     }
-    return initialSum;
+    return { finalEven, finalOdd }
 }
 
-
-let i = sum([50,5])
-console.log(i)
+let a = even([2, 5, 65, 41, 23, 201, 222, 8520,136])
+console.log(a)
+let b = even([4,01, 1, 664, 8, 54, 555, 7, 121, 0, 5545, 10, 41])
+console.log(b)
